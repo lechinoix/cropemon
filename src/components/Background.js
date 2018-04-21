47b44fa44462f1d2bbf7c1d3ade97188aa34e4cf
lechinoix <nicolasn@theodo.fr>
2018-04-21 17:11:56 +0200

    Fix background style

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const backgroundStyle = {
+  alignItems: 'stretch',
   backgroundPosition: 'center',
   backgroundSize: 'cover',
-  height: '100vh',
-  width: '100vw',
+  bottom: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  left: 0,
+  overflow: 'hidden',
   position: 'absolute',
+  right: 0,
+  top: 0,
   zIndex: -1,
 }
 
