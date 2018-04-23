8f146fa78ad8d45ecf3ce88b998b7506338846ec
lechinoix <nicolasn@theodo.fr>
2018-04-23 20:09:18 +0200

    Remove map

--- src/pages/PokeMap/Map.js
+++ src/pages/PokeMap/Map.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { GoogleMap, Marker, withScriptjs, withGoogleMap } from "react-google-maps"
-import { compose, withProps } from "recompose"
-import mapStyle from './mapStyle.json';
 
 const containerStyle = {
   bottom: 0,
@@ -13,25 +10,8 @@ const containerStyle = {
 
 const MAPS_API_KEY = 'AIzaSyCFs36lo5GXQkDPVecvhqOxzIXV3U9kEcw';
 
-const enhanceComponent = compose(
-  withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${MAPS_API_KEY}&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={containerStyle} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap,
-);
-
 const Map = (props) => (
-  <GoogleMap
-    defaultZoom={15}
-    defaultCenter={{ lat: 48.926, lng: 2.32 }}
-    defaultOptions={{ styles: mapStyle }}
-  >
-    <Marker position={{ lat: 48.926, lng: 2.32 }} />
-  </GoogleMap>
+  <h1>This will be the map</h1>
 )
 
-export default enhanceComponent(Map);
+export default Map;
